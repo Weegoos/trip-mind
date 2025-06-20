7b5386b75812119a9831302dacc95560817972ba
Weegoos <batyr.ashim05@mail.ru>
2025-06-20 15:06:58 +0500

    TripAndMind: The Header.tsx has been updated

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,24 @@
-export default function Header () {
-    return (
-        <div>
-            <header>Шапка профиля</header>
+"use client";
+
+export default function Header() {
+  const companyName = process.env.NEXT_PUBLIC_COMPANY_NAME;
+
+  function handleClick() {
+    console.log("Clicked");
+  }
+  return (
+    <div>
+      <header className="p-8 md:grid grid-cols-3 grid-rows-1 gap-4">
+        <div className="text-2xl hidden md:block">{companyName}</div>
+        <div className="justify-center gap-4 md:flex hidden">
+          <button onClick={handleClick}>Все туры</button>
+          <button onClick={handleClick}>Отзывы</button>
+          <button onClick={handleClick}>Контакты</button>
         </div>
-    )
+        <div className="">
+            <span className="mdi mdi-menu text-3xl md:flex justify-end"></span>
+        </div>
+      </header>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header";
+
 export default function Home() {
   return (
     <div className="text-white bg-[url('https://images.unsplash.com/photo-1515310787031-25ac2d68610d?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]  ">
-      
+      <Header />
     </div>
   );
 }
